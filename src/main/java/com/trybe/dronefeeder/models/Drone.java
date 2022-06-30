3b9b396f22cf5669a7e15bb9ba17c5acb90bf355
renatomak <renato.mark.silva@gmail.com>
2022-06-30 00:31:57 -0300

    fix: included getId

--- src/main/java/com/trybe/dronefeeder/models/Drone.java
+++ src/main/java/com/trybe/dronefeeder/models/Drone.java
@@ -1,4 +1,4 @@
-package com.trybe.dronefeeder.model;
+package com.trybe.dronefeeder.models;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -14,6 +14,14 @@ public class Drone implements Serializable {
     private Long id;
     private String brand;
     private String model;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getBrand() {
         return brand;
