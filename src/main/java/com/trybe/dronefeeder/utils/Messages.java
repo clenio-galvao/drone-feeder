5ee7af1ff898b900fe1c9bbd339bf096580ee7e7
renatomak <renato.mark.silva@gmail.com>
2022-07-07 11:26:19 -0300

    fix: error message

--- src/main/java/com/trybe/dronefeeder/utils/Messages.java
+++ src/main/java/com/trybe/dronefeeder/utils/Messages.java
@@ -3,8 +3,8 @@ package com.trybe.dronefeeder.utils;
 public class Messages {
     public static final String VALIDATION_BRAND_IS_REQUIRED = "Brand is required.";
     public static final String VALIDATION_BRAND_IS_EMPTY = "Brand cannot be empty.";
-    public static final String VALIDATION_BRAND_SIZE = "Brand must have a minimum of 3 characters and a maximum of 100.";
-    public static final String VALIDATION_MODEL_SIZE = "Model must have a minimum of 3 characters and a maximum of 240.";
+    public static final String VALIDATION_BRAND_SIZE = "Brand must have a minimum of 3 characters and a maximum of 50.";
+    public static final String VALIDATION_MODEL_SIZE = "Model must have a minimum of 3 characters and a maximum of 50.";
     public static final String VALIDATION_MODEL_IS_REQUIRED = "Model is required.";
     public static final String VALIDATION_MODEL_IS_EMPTY = "Model cannot be empty.";
 
